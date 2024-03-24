d697a034d3effbfcac2ce3875f5e7681aa8d61e6
Aubin Koenigssohn <mvogtsinga@gmail.com>
2024-03-24 17:53:00 +0000

    connect heroku and project

--- weekendfoodfoot/settings.py
+++ weekendfoodfoot/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-i_y(s+ca5$)-n0kzfh#p9$3$#o0tpx)u$%x+(sm-hy)59on=sq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
